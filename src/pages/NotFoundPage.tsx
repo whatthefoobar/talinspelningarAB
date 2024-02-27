4683dd59d63d28f463920e20aba68714910c244c
whatthefoobar <irina.mntn@gmail.com>
2024-02-27 15:13:22 +0100

    made a minimal not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 const NotFoundPage = () => {
   return (
-    <div style={{ marginTop: "10vh", textAlign: "center", fontSize: "2rem" }}>
-      NotFoundPage
-    </div>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Typography variant="h2" color="error" gutterBottom>
+        404
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        Oops! Page not found.
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        The page you are looking for might have been removed, had its name
+        changed, or is temporarily unavailable.
+      </Typography>
+      <Button variant="contained" component={Link} to="/" color="primary">
+        Go to Home
+      </Button>
+    </Box>
   );
 };
 
