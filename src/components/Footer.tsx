02a71c8f08443db3d066a6f52eefff7d8ab8635b
Irina Munteanu Bäck <69626975+whatthefoobar@users.noreply.github.com>
2024-08-15 12:21:43 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Link from 'next/link';
 import { Grid, Typography } from "@mui/material";
 import Image from "next/image";
 
