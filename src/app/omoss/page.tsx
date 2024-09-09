50e9063ec7557ea16bf9f3d17c5ff2b11ac6fa20
whatthefoobar <irina.mntn@gmail.com>
2024-09-09 11:55:28 +0200

    added text indent to paragraphs in about us

--- src/app/omoss/page.tsx
+++ src/app/omoss/page.tsx
@@ -25,14 +25,22 @@ const AboutPage: React.FC = () => {
           fontSize: "1.3rem",
           marginBottom: "16px",
           textAlign: "left",
+          textIndent: "30px",
         }}
       >
         Vi har en stor stab av professionella uppläsare inom olika kategorier.
         Många med mångårig erfarenhet. Våra studios är specialutrustade för just
         uppläsning, vilket gör inspelningsarbetet effektivt och
         kostnadsbesparande.
-        <br />
-        <br />
+      </Typography>
+      <Typography
+        style={{
+          fontSize: "1.3rem",
+          marginBottom: "16px",
+          textAlign: "left",
+          textIndent: "30px",
+        }}
+      >
         Vi har mer än 30 års erfarenhet av inspelning av böcker, de senaste 10
         åren har vi spelat in 1.000-1.500 titlar per år. Talins pelningar har
         hög kompetens inom respektive fackområde och modermålsinläsare i alla
